2a0a55158ce05c95157ca91ffb9e99b840685382
Pankajahura <pankaj.soni@ahurasense.com>
2026-01-08 17:46:08 +0530

    remove .env

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json())
 
 app.get('/health', (req, res) => {
   res.status(200).json({ status: 'ok' });
